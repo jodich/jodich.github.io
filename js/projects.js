f7e49febf97fb6df190682165ec72b048b8ed32e
jodich <jodi_choo@hotmail.com>
2018-09-03 22:39:42 +0800

    test link

---
+++ js/projects.js
@@ -0,0 +1,32 @@
+var projects = [
+    {
+        thumbnail: "/media/piss-thumbnail.jpg",
+        title: "PiSS",
+        description: "a Satrical Suveillance System",
+        tags: ["concept", "ux/ui", "branding", "advertising"]
+    },
+    {
+        thumbnail: "/media/1-thumbnail.jpg",
+        title: "$1",
+        description: "a publication reflecting on Singapore's rising cost of living",
+        tags: ["concept", "ux/ui", "branding", "advertising"]
+    },
+    {
+        thumbnail: "/media/earth-thumbnail.jpg",
+        title: "Earthy",
+        description: "A Satrical Suveillance System",
+        tags: ["concept", "ux/ui", "branding", "advertising"]
+    },
+    {
+        thumbnail: "/media/piss-thumbnail.jpg",
+        title: "PiSS",
+        description: "A Satrical Suveillance System",
+        tags: ["concept", "ux/ui", "branding", "advertising"]
+    },
+    {
+        thumbnail: "/media/piss-thumbnail.jpg",
+        title: "PiSS",
+        description: "A Satrical Suveillance System",
+        tags: ["concept", "ux/ui", "branding", "advertising"]
+    }
+]
---
+++ js/script.js
@@ -0,0 +1,26 @@
+$( document ).ready(function() {
+
+    $('.nav-links a').hover(
+        (event) => $(event.currentTarget).toggleClass('nav-link-active')
+    )
+
+    for (let i = 0; i < projects.length; i++) {
+        let project = projects[i];
+
+        // thumbnail
+        let $projectThumbnail = $("<div></div>").addClass("col m12 l6 project-box");
+        let $projectImg = $("<img>").attr('src', project.thumbnail).addClass("project-thumbnail");
+        $projectThumbnail.append($projectImg);    
+
+        let $projectOverlay = $("<div></div>").addClass("project-overlay");
+        let $projectTitle = $("<div></div>").addClass("title").text(project.title);
+        let $projectDescription = $("<p></p>").addClass("description").text(project.description);
+        $projectOverlay.append($projectTitle);
+        $projectOverlay.append($projectDescription);
+
+        $projectThumbnail.append($projectOverlay);    
+
+        
+        $("#projects").append($projectThumbnail);
+    }
+});
